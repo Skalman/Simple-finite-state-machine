5adbdb553152f6be7d975887e5f077a9599e6c8e
Dan Wolff <fondiller@gmail.com>
2012-03-19 17:17:07 +0100

    f4c46e from minimal: throw implicitly

--- minimal-fsm-modern.js
+++ minimal-fsm-modern.js
@@ -7,9 +7,11 @@ function Fsm(current_state, events) {
 	"use strict";
 	return function (event) {
 		// a TypeError will be thrown if the event doesn't exist
-		if (!~events[event].from.split(" ").indexOf(current_state)) {
-			throw event;
+		if (~events[event].from.split(" ").indexOf(current_state)) {
+			current_state = events[event].to;
+		} else {
+			// throw a TypeError: a string cannot be used as a function
+			event();
 		}
-		current_state = events[event].to;
 	};
 }
